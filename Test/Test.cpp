f0f4e0567eb179836b8ebaa0bf73235fc1a0fd93
ilya <ilya.21062001@gmail.com>
2019-09-23 23:17:46 +0300

    puch

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,20 +1,142 @@
-﻿// Test.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+using namespace std;
 
-#include <iostream>
+int* make_arr(int* arr, int n) {
+	for (int i = 0; i < 32; ++i) {
+		arr[i] = 0;
+	}
+	int j = 0;
+	while (n != 0) {
+		arr[j] = n % 2;
+		j += 1;
+		n /= 2;
+	}
+	
+	return arr;
+}
+
+void show_arr(int* arr) {
+	for (int i = 31; i > 0; i--)
+		cout << arr[i];
+	cout << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	/*
+	cout << ~k << endl; //побитовое интвертирование
+	cout << (0 & 1) << endl; //битовое и
+	cout << (1 | 1) << endl; //битовое или
+	cout << (1 ^ 1) << endl; //исключающее или
+
+	cout << (3 << 2) << endl;//побитовый сдвиг
+	cout << (4 >> 2) << endl;//право
+	cout << endl;
+	cout << (1 && 4) << endl;//логическое и
+	cout << (3 || 4) << endl;//логическое или
+	*/
+	int n = 0;
+	int num = 0;
+	int k = 0;
+	int r = 0;
+	int a[32];
+	int b[32];
+	int n2 = 0;
+
+	cout << "Select the task: ";
+	int task = 0; cin >> task;
+	if(task==2){//вывод подчисла в двоич
+		cin >> num;
+		cin >> n;
+		cin >> k;
+		for (int i = 0; i < n; i++) {
+			num /= 2;
+		}
+		for (int i = 0; i < k; i++) {
+			r += pow((num % 2), i);
+			num /= 2;
+		}
+		cout << r;
+	}
+	else if (task == 3) {//вывод числа в двоич
+		cin >> num;
+		make_arr(a, abs(n));
+		show_arr(a);
+	}
+	else if (task == 4) {
+		cin >> n;
+		if (n < 0)
+			n -= 1;
+		make_arr(a,n);
+		a[31] = 1;
+		show_arr(a);
+	}
+	else if (task == 5) {//цикл сдвиг
+		cin >> n;
+		cin >> k;
+		make_arr(a, n);
+		show_arr(a);
+		for (int i = 0; i < abs(k); i++) {
+			if (k > 0) {
+				int end = a[0];
+				for (int j = 0; j < 31; j++) {
+					a[j] = a[j + 1];
+				}
+				a[31] = end;
+			}
+			if (k < 0) {
+				int end = a[31];
+				for (int j = 31; j > 0; j--) {
+					a[j] = a[j - 1];
+				}
+				a[0] = end;
+			}
+
+		}
+		for (int i = 31; i >= 0; i--) {
+			cout << a[i];
+		}
+	}
+	else if (task == 6) {
+		cin >> n;
+		cin >> n2;
+
+		make_arr(a, n);
+		make_arr(b, n2);
+		show_arr(a);
+		show_arr(b);
+
+		for (int i = 31; i > 0; i--) {
+			cout << (a[i] == b[i]);
+		}
+	}
+	if (task == 7) {
+		cin >> n;
+		make_arr(a, n);
+		show_arr(a);
+		for (int i = 0; i < 32; i++) {
+			a[i] = !a[i];
+		}
+		show_arr(a);
+	}
+	if (task == 8) {
+		cin >> n;
+		cin >> n2;
+		make_arr(a, n);
+		make_arr(b, n2);
+		show_arr(a);
+		show_arr(b);
+		for (int i = 0; i < 32; i+=2) {
+			a[i] = b[i];
+		}
+		for (int i = 1; i < 32; i += 2) {
+			b[i] = a[i];
+		}
+		show_arr(a);
+		show_arr(b);
+	}
+	else
+		cout << "Incorrect task number!";
+
+	return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
