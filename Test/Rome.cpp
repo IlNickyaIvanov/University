d71fb59834cd567cc9c33f5871a1114520fc47d3
ilya <ilya.21062001@gmail.com>
2019-09-25 09:09:14 +0300

    roman

---
+++ Test/Rome.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+#include <iomanip>
+using namespace std;
+
+string to_roman(unsigned int value)
+{
+	struct romandata_t { unsigned int value; char const* numeral; };
+	const struct romandata_t romandata[] =
+	{
+	{1000, "M"}, {900, "CM"},
+	{500, "D"}, {400, "CD"},
+	{100, "C"}, { 90, "XC"},
+	{ 50, "L"}, { 40, "XL"},
+	{ 10, "X"}, { 9, "IX"},
+	{ 5, "V"}, { 4, "IV"},
+	{ 1, "I"},
+	{ 0, NULL} // end marker
+	};
+
+	string result;
+	for (const romandata_t* current = romandata; current->value > 0; ++current)
+	{
+		while (value >= current->value)
+		{
+			result += current->numeral;
+			value -= current->value;
+		}
+	}
+	return result;
+}
+
+int main()
+{
+	int n; cin >> n;
+	to_roman(n);
+	system("PAUSE");
+	return 0;
+}
